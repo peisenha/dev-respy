cd78e965fb19e1cc416cf5bfd45304355b041c67
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2020-01-03 10:34:38 +0100

    cleaned

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+
 # In this script we only have explicit use of MPI as our level of parallelism. This needs to be
 # done right at the beginning of the script.
 update = {'NUMBA_NUM_THREADS': '1', 'OMP_NUM_THREADS': '1', 'OPENBLAS_NUM_THREADS': '1',
@@ -8,12 +9,14 @@ os.environ.update(update)
 import respy as rp
 import numpy as np
 
-params, options, df = rp.get_example_model("robinson", with_data=True)
+from respy.config import EXAMPLE_MODELS
 
-crit_func = rp.get_crit_func(params, options, df)
+for model in ["kw_97_basic"]:
+    print(model)
+    params, options, df = rp.get_example_model(model, with_data=True)
 
-for delta in [0.001, 0.5]:
-    print("\n\n")
-    params.loc[("shocks_sdcorr", "sd_hammock"), :] = delta
-    fval = crit_func(params)
-    print(delta, fval)
+    simulate = rp.get_simulate_func(params, options)
+    simulate(params)
+
+    crit_func = rp.get_crit_func(params, options, df)
+    crit_func(params)
