6bf1dd228bc12c743bbd15b669db6e40b35950f6
peisenha <peisenha@protonmail.com>
2020-10-24 14:45:48 +0200

    edits, working

--- extend_occupations.py
+++ extend_occupations.py
@@ -10,25 +10,28 @@ params, options, data = rp.get_example_model("kw_94_two")
 
 NUM_OCCUPATIONS = 3
 
-# TODO: Needs to be larger than previous ones.
+# TODO: Needs to be larger than previous ones?
 assert NUM_OCCUPATIONS > 0
 
 params_occ = add_generic_occupations(params, NUM_OCCUPATIONS)
 params_occ = construct_shocks_sdcorr(params_occ)
 
 # TODO: We are now working on options part based on the revised params.
-options["n_periods"] = 10
+options["n_periods"] = 10 # debugging only.
 
-options["core_state_space_filters"] = [
-    "period > 0 and exp_{choices_w_exp} == period and lagged_choice_1 != '{choices_w_exp}'",
-    "period > 0 and exp_a + exp_b + exp_c + exp_edu == period and lagged_choice_1 == '{choices_wo_exp}'",
-    "period > 0 and lagged_choice_1 == 'edu' and exp_edu == 0",
-    "lagged_choice_1 == '{choices_w_wage}' and exp_{choices_w_wage} == 0",
-    "period == 0 and lagged_choice_1 == '{choices_w_wage}'",
-]
+# TODO: Updating filters ...
+_, occupations = _get_choices_occupations(params_occ)
+substring = ""
+for occupation in occupations:
+    substring += f"exp_{occupation} + "
 
+substring = substring[:substring.rfind("+") - 1]
+substring = f"period > 0 and {substring} + exp_edu == period "
+substring += "and lagged_choice_1 == '{choices_wo_exp}'"
 
+options["core_state_space_filters"][1] = substring
 
+# TODO: Updating covariates ...
 entries_to_remove = list()
 for key_ in options["covariates"].keys():
     if key_.startswith("exp_"):
