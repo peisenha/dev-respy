8671656cda3bb44a8204362c1b9cf58c846693fe
peisenha <peisenha@protonmail.com>
2020-10-26 15:27:34 +0100

    cleanup continued, working

--- test_extend_occupations.py
+++ test_extend_occupations.py
@@ -6,6 +6,14 @@ import numpy as np
 from auxiliary import scaling_model_specification
 
 
+def get_random_request():
+    add_sample = [None, 1, 2, 3, 4]
+    add_occ, add_types = np.random.choice(add_sample, size=2)
+    num_periods = np.random.choice(range(1, 10))
+    model = np.random.choice(["kw_97_basic", "kw_94_one", "kw_97_extended"])
+    return model, num_periods, add_occ, add_types
+
+
 def test_1():
     params, options = scaling_model_specification("kw_94_two", 10, 3)
     df = rp.get_simulate_func(params, options)(params)
@@ -20,3 +28,8 @@ def test_2():
         args = (model, num_periods, add_occ, add_types)
         params, options = scaling_model_specification(*args)
         rp.get_simulate_func(params, options)(params)
+
+
+def test_3():
+    params, options = scaling_model_specification(*get_random_request())
+    rp.get_simulate_func(params, options)(params)
